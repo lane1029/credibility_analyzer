c3bda835a8c069779677dbc39b43df3ffa306391
Laney Marsh <180482146+lane1029@users.noreply.github.com>
2024-11-15 16:49:42 +0000

    added functions to fetch and return assistants

--- backend/assistants.js
+++ backend/assistants.js
@@ -40,47 +40,16 @@ export async function getAssistants(topic) {
   try {
     const assistantList = await getAssistantList();
     // Select the assistant from the assistant list with the name containing the topic
-    const topicAssistant = assistantList.find(assistant => assistant.name.includes(topic));
-    const biasAssistant = assistantList.find(assistant => assistant.name.includes('bias'));
+    const topicAssistant = assistantList.find(assistant => assistant.name.toLowerCase().includes(topic));
+    const biasAssistant = assistantList.find(assistant => assistant.name.toLowerCase().includes('bias'));
 
     // Retrieve the assistants using the assistant IDs
     const factCheckAssistant = await retrieveAssistant(topicAssistant.id);
     const biasCheckAssistant = await retrieveAssistant(biasAssistant.id);
-    return factCheckAssistant, biasCheckAssistant;
+    return [factCheckAssistant, biasCheckAssistant];
   } catch (error) {
     console.error('Error fetching assistant from OpenAI API:', error.response?.data || error.message);
     throw error;
   }
 
 }
-
-// Function to create a thread
-// async function create_thread() {
-//   try{
-//     let response = await openai.beta.threads.create()
-//     return response;
-//   } catch (error) {
-//     console.error('Error creating thread:', error.response?.data || error.message);
-//     throw error;
-//   }
-// }
-
-// // Function to clean the response from the OpenAI API
-// // Removes citation placeholders from the response
-// function clean_response(response) {
-//   return response.replace(/【\d+:\d+†source】/g, ''); // Removes citation placeholders
-// }
-
-// // Function to get all messages from the thread and filter on just the most recent response
-// async function get_all_messages(messages) {
-//   let full_response = JSON.stringify(messages);
-//   full_response = JSON.parse(full_response);
-
-//   // Get the most recent response from the assistant
-//   let content = full_response.data[0].content
-//   let response_message = content[0].text.value;
-
-//   // Clean the response for citation placeholders
-//   const cleanedMessage = clean_response(response_message);
-//   return cleanedMessage;
-// }
