8ca1f39547fb656f0b14b9f138c7cb43f0d64f51
Laney Marsh <180482146+lane1029@users.noreply.github.com>
2024-11-18 00:52:13 +0000

    removing consolde message in server.js

--- backend/server.js
+++ backend/server.js
@@ -54,7 +54,6 @@ app.post('/api/analyze', async (req, res) => {
     const biasAnalysis = await analyze(textContent, assistants[1]);
     const factAnalysis = await analyze(textContent, assistants[0]);
     const credibilityAnalysis = await summarizeCredibilityAnalysis(biasAnalysis, factAnalysis);
-    console.log(credibilityAnalysis);
     return res.json({ "credibilityResult": credibilityAnalysis, "biasResult" : biasAnalysis , "factResult" : factAnalysis });
   } catch (error) {
     console.error('Error analyzing URL:', error);
