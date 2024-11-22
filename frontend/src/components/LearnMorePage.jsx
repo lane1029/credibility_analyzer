b887be5ff994aa501d0705c173d79942f803f9ed
Laney Marsh <180482146+lane1029@users.noreply.github.com>
2024-11-22 17:03:52 +0000

    removed placeholder text on learning page

--- frontend/src/components/LearnMorePage.jsx
+++ frontend/src/components/LearnMorePage.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import LearningTabs from './LearningTabs';
-import { Link } from 'react-router-dom';
 
 function LearnMorePage() {
     return (
         <div style={pageStyle}>
-            <h1>Welcome to the Second Page</h1>
-            <p>This is another page in your React app.</p>
             <LearningTabs />
-            <Link to="/" style={linkStyle}>
-                Go Back to Home
-            </Link>
         </div>
     );
 }
@@ -19,14 +13,6 @@ const pageStyle = {
     textAlign: 'center',
     padding: '20px',
     fontFamily: 'Shippori Antique B1, sans-serif',
-};
-
-const linkStyle = {
-    marginTop: '20px',
-    display: 'inline-block',
-    textDecoration: 'none',
-    color: '#007bff',
-    fontSize: '1.2rem',
 };
 
 export default LearnMorePage;
