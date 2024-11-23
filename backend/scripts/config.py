9c1cbe47bc77c75c0c77e99d86e9aa9d45546f2e
Laney Marsh <180482146+lane1029@users.noreply.github.com>
2024-11-23 00:52:51 +0000

    adding comments to config.py

--- backend/scripts/config.py
+++ backend/scripts/config.py
@@ -1,3 +1,5 @@
+# Description: Configuration file for the backend server
+
 import os
 from dotenv import load_dotenv
 
@@ -6,14 +8,15 @@ load_dotenv()
 
 class Config:
     OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")  # OpenAI API key
-    # MongoDB URI, fallbacks to a default if not specified
-    MONGO_URI = os.getenv("MONGODB_URI")
+    MONGO_URI = os.getenv("MONGODB_URI") # MongoDB URI
     DB_NAME = os.getenv("DB_NAME", "CredAnalyzer")  # default database name
     ARTICLE_COLLECTION_NAME = os.getenv("COLLECTION_NAME", "articles")  # default collection name
     METADATA_COLLECTION_NAME = os.getenv("METADATA_COLLECTION_NAME", "metadata")  # default
     PORT = int(os.getenv("PORT", 5000))  # Port for web server if needed
+    # URL for the USAFacts website to be used in the web scraper
     MAIN_URL = "https://usafacts.org/articles/category/health"
     BASE_URL = "https://usafacts.org/"
+    # For testing scraper
     OUTPUT_DIR = "backend/data/temp_outputs"
     HEADERS = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
